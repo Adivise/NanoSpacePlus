e842190eacff9d29864dd5916185d5186a3dfa9a
aadiyouknow <72033983+AADI0009@users.noreply.github.com>
2022-09-30 23:37:24 +0530

    Update sharder.js

--- sharder.js
+++ sharder.js
@@ -1,6 +1,12 @@
 const { ShardingManager } = require('discord.js'); //imports the sharding manager
 
-const manager = new ShardingManager('./index.js', { token: 'paste your token' }); //paste your token here
+const manager = new ShardingManager('./index.js', { 
+  token: 'paste your token', //paste your token here
+  respawn: true,
+  autoSpawn: true,
+  totalShards: 1, //amount of shards
+  shardList: "auto", //edit it only if you know what are you doing
+});
 
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 
