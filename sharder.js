f9f99fb7a71fd67e75faccf3a2aef7bfd9d4cb34
Nanotect <61177761+Adivise@users.noreply.github.com>
2022-10-01 15:38:59 +0700

    dotenv

--- sharder.js
+++ sharder.js
@@ -1,7 +1,8 @@
 const { ShardingManager } = require('discord.js'); //imports the sharding manager
+require("dotenv").config();
 
 const manager = new ShardingManager('./index.js', { 
-  token: 'paste your token', //paste your token here
+  token: process.env.TOKEN || "YOUR_BOT_TOKEN", //paste your token here
   respawn: true,
   autoSpawn: true,
   totalShards: 1, //amount of shards
