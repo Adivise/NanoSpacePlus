60bf16b7d4adfacf6586fdec5c5dae2bcfa69d11
Jatin <75193966+TEDDY6969@users.noreply.github.com>
2022-10-20 16:09:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const express = require("express");
+const app = express()
+ 
+app.get('/', (req, res) => {
+  res.send("Beatrix is on as always")
+})
+ 
+app.listen(3000, () => {
+  console.log("Beatrix Project is Working!! hehe")
+});
 const MainClient = require("./nanospace.js");
 const client = new MainClient();
 
